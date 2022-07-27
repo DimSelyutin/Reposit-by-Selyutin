b5cba51bf52ad6678f81e74429061668dfb61189
DimSelyutin <102747240+DimSelyutin@users.noreply.github.com>
2022-07-28 01:26:26 +0300

    Create Task03.java

---
+++ main/Task03.java
@@ -0,0 +1,10 @@
+package by.epam.tr.main;
+
+
+import java.util.Scanner;
+
+public class Task02{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+    }
+}
