9f64f6e5634f7a7c5893821fe80dc66b53ac116e
DimSelyutin <102747240+DimSelyutin@users.noreply.github.com>
2022-07-31 15:13:28 +0300

    Update Task25.java

--- main/Task25.java
+++ main/Task25.java
@@ -8,13 +8,17 @@ import java.util.Scanner;
 public class Task25{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        double a = scanner.NextDouble();
-		double b = scanner.NextDouble();;
-		double c = scanner.NextDouble();;
-		double x1 = (-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / 2 * a;
-		double x2 = (-b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / 2 * a;
+        System.out.println("Введите a,b и c:");
+        System.out.print("a :");
+        double a = scanner.nextDouble();
+        System.out.print("b :");
+	double b = scanner.nextDouble();
+        System.out.print("c :");
+	double c = scanner.nextDouble();
+	double x1 = (-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / 2 * a;
+	double x2 = (-b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / 2 * a;
 
-		System.out.printf("a, b и c равны %.0f, %.0f и %.0f\n", a, b, c);
-		System.out.printf("Корни x1 и x2 = %.2f и %.2f", x1, x2);
+	System.out.printf("a, b и c равны %.0f, %.0f и %.0f\n", a, b, c);
+	System.out.printf("Корни x1 и x2 = %.2f и %.2f", x1, x2);
     }
 }
